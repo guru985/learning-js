b9e0a1ebfc0e85397f7f5b8964206ecfa8d1f229
guru <gurudev.c@gai.co.in>
2021-03-02 15:13:06 +0530

    modify the inex.html and inex.js files

---
+++ index.js
@@ -0,0 +1,15 @@
+var country = 'India';
+var continent = 'Asia';
+var population = '127cr';
+
+console.log(country, continent, population);
+
+
+// =============================================================================
+
+var isIsland = true;
+var language;
+var country = 'India';
+var population = 12700000;
+
+console.log(typeof isIsland, typeof population, typeof country, typeof language);
