750c1d6d6340bcf7cfde93c35ce34fdfaa034cc8
guru <gurudev.c@gai.co.in>
2021-03-03 10:51:27 +0530

    modiy the index.js files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var country = 'India';
 var continent = 'Asia';
-var population = '127cr';
+var population = '136.64cr';
 
 console.log(country, continent, population);
 
@@ -10,6 +10,28 @@ console.log(country, continent, population);
 var isIsland = true;
 var language;
 var country = 'India';
-var population = 12700000;
+var population = 13600000.64;
 
 console.log(typeof isIsland, typeof population, typeof country, typeof language);
+
+
+
+// ===================================================================================
+
+// var example
+
+var officeEmployNumber = 27;
+officeEmployNumber = 45;
+
+// const example
+const officeEmployName = 'Gurdev';
+
+
+// let example
+
+{
+	let officeEmployNumber = 75;
+	officeEmployNumber = 60;
+}
+
+console.log(officeEmployName, officeEmployNumber);
