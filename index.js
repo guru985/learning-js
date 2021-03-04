29547f27697d4956b22283aec9461c249b9cf09a
guru <gurudev.c@gai.co.in>
2021-03-04 17:35:10 +0530

    Add new task in js file

--- index.js
+++ index.js
@@ -35,3 +35,90 @@ const officeEmployName = 'Gurdev';
 }
 
 console.log(officeEmployName, officeEmployNumber);
+
+
+
+
+// ===============================================================================================
+
+var population = 1300000000;
+
+if(population >= 350000000){
+	console.log("Portugal's population is above average");
+}else   {
+	console.log("Portugal's population is 22 million below average");
+} 
+
+
+// ==========================================================================================================
+
+var numberOne = '9';
+var numberTwo = '5';
+
+var total = numberOne - numberTwo;
+
+console.log(total);
+
+// ================================================
+
+var one = '19';
+var two = '13';
+var three = '17'
+
+var numverToatl = one - two + three
+
+console.log (numverToatl);
+
+// ====================================================
+
+var oneS = '19';
+var twoT = '13';
+var threeL = 17;
+
+var numvberToatl = oneS - twoT + threeL
+
+console.log (numvberToatl);
+
+// ========================================================
+
+var newnumber = '123';
+var newnumberTwo = 57;
+
+var newTotalNumber = newnumber < newnumberTwo
+console.log(newTotalNumber);
+
+// =================================================================
+
+var plusFive = 5;
+var plusSix = 6;
+var plusfour = '4';
+var plusNine = 9;
+var minsFoue = 4;
+var minsTwo = 2;
+
+var numbersTotal = plusFive + plusSix + plusfour + plusNine - minsFoue - minsTwo
+console.log(numbersTotal);
+
+
+// =====================================================================================================
+
+const countryPopulation = 1270000000;
+const countrySplitHelf = countryPopulation/2;
+console.log(countrySplitHelf);
+
+
+// ========================================================================================================
+
+let ToatlPopulation = 1270000000;
+ToatlPopulation++;
+
+console.log(ToatlPopulation);
+
+// ============================================================================================================
+
+let finlandPopulation = 600000;
+let India = 1270000000;
+
+let myCountryPopulation = finlandPopulation < India
+
+console.log("My country opulation bigger then finland"., + '', myCountryPopulation);
